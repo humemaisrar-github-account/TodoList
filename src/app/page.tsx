019d7de46ebb0dd3f683a8e3b4066a09b4936f84
humemaisrar-github-account <162582825+humemaisrar-github-account@users.noreply.github.com>
2024-11-03 13:53:25 -0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import TodoList from "@/components/todo-list"; 
+
+export default function Home() {
+  return (
+    <div >
+      <TodoList />
+    </div>
+  );
+}
